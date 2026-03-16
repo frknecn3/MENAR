38fe5b84762ddb584e931b21eb494114a926829e
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-16 13:10:42 +0300

    refactor(axios): remove unnecessary headers from axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://www.kap.org.tr/tr/api/",
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+    timeout: 10000
 })
 
 export default api
