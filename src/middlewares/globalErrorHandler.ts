f879ccc60b1b6966a5f07172caf518f4520a7e75
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-16 12:39:42 +0300

    feat(error-handler): enhance global error handler with AppError class

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,11 +1,33 @@
 import type { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
+export class AppError extends Error {
+
+    statusCode:number;
+    data?:unknown;
+    details?:any
+
+
+    constructor(message:string,statusCode:number,data:unknown,details:any){
+        super(message)
+
+        this.statusCode = statusCode,
+        this.data = data,
+        this.details = details
+    }
+}
+
 const handler: ErrorRequestHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     console.error(err)
 
-    // if(err instanceof AppError)
+    if(err instanceof AppError) {
+        return res.status(err.statusCode).send({
+            success: false,
+            message: err.message,
+            details: err.details
+        })
+    }
 
-    res.status(err.statusCode || 500).json({
+    return res.status(err.statusCode || 500).json({
         success: false,
         message: err.message || "Internal Server Error"
     })
