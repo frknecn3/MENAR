84e241bb3078dcccba2a7687f01a166522567efd
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-16 13:10:54 +0300

    refactor(globalErrorHandler): improve error handling structure and formatting

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,25 +1,24 @@
+import { AxiosError } from "axios";
 import type { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
 export class AppError extends Error {
 
-    statusCode:number;
-    data?:unknown;
-    details?:any
+    statusCode: number;
+    data?: unknown;
+    details?: any
 
 
-    constructor(message:string,statusCode:number,data:unknown,details:any){
+    constructor(message: string, statusCode: number, data: unknown, details: any) {
         super(message)
 
         this.statusCode = statusCode,
-        this.data = data,
-        this.details = details
+            this.data = data,
+            this.details = details
     }
 }
 
-const handler: ErrorRequestHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err)
-
-    if(err instanceof AppError) {
+const handler: ErrorRequestHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
         return res.status(err.statusCode).send({
             success: false,
             message: err.message,
@@ -27,10 +26,36 @@ const handler: ErrorRequestHandler = (err: any, req: Request, res: Response, nex
         })
     }
 
-    return res.status(err.statusCode || 500).json({
-        success: false,
-        message: err.message || "Internal Server Error"
-    })
+    if (err instanceof AxiosError) {
+        console.log(err.response?.status, err.response?.statusText, err.request.res.responseUrl);
+
+        return res.status(err.response?.status || 500).send({
+            success: false,
+            message: err.message,
+        })
+
+    }
+
+    if (err instanceof Error) {
+        // mesajın içindeki ilk 3 haneli sayıyı bul
+        const match = err.message.match(/\d{3}/);
+
+        // eğer sayı bulunduysa onu kullan, bulunamadıysa 500'e dön
+        const extractedStatus = match ? parseInt(match[0]) : 500;
+
+        // geçerli bir HTTP kodu mu kontrolü
+        const finalStatus = (extractedStatus >= 400 && extractedStatus < 600)
+            ? extractedStatus
+            : 500;
+
+        return res.status(finalStatus).json({
+            success: false,
+            message: err.message || "Internal Server Error"
+        });
+    }
+
+    // hiçbiri değilse en genel hata
+    res.status(500).json({ success: false, message: "Bilinmeyen Hata" });
 }
 
 export default handler;
