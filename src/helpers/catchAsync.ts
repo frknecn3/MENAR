6d981d46614d9100104adac4146e695195faef9a
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-16 13:10:48 +0300

    fix(catchAsync): improve error handling by explicitly passing error to next

--- src/helpers/catchAsync.ts
+++ src/helpers/catchAsync.ts
@@ -3,5 +3,6 @@ import type { NextFunction, Request, RequestHandler, Response } from "express"
 export const c =
     (fn: RequestHandler): RequestHandler =>
         (req: Request, res: Response, next: NextFunction) => {
-            Promise.resolve(fn(req, res, next)).catch(next);
+            Promise.resolve(fn(req, res, next)).catch((err)=>{next(err)
+            });
         };
