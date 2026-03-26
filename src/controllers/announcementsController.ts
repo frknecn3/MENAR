83ae52b6fbc1f457f733084a5522e026bdb76e29
Furkan Ercan <furkanduhannuman@gmail.com>
2026-03-26 11:38:24 +0300

    feat(announcementsController): updated api links and add getAnnouncement controller

--- src/controllers/announcementsController.ts
+++ src/controllers/announcementsController.ts
@@ -3,10 +3,12 @@ import type { Request, Response } from 'express'
 import { c } from "../helpers/catchAsync";
 import api from "../api/axios";
 import { AppResponse } from "../helpers/response";
+import { parseKapHtmlToMarkdown } from "../helpers/cleanHTML";
+import { analyzeKapNotification } from "../llm/analystService";
 
 export const getAllAnnouncements: RequestHandler = c(async (req: Request, res: Response) => {
     console.log("announcements trigger")
-    const response = await api.post('disclosure/list/main', {
+    const response = await api.post('api/disclosure/list/main', {
         fromDate: "16.03.2026",
         memberTypes: ["IGS", "DDK"],
         toDate: "16.03.2026"
@@ -14,5 +16,17 @@ export const getAllAnnouncements: RequestHandler = c(async (req: Request, res: R
 
     console.log("Cevap: ", response.data)
 
-    return AppResponse(res,'Duyurular başarıyla getirildi.', response.data)
+    return AppResponse(res, 'Duyurular başarıyla getirildi.', response.data)
+})
+
+export const getAnnouncement: RequestHandler = c(async (req: Request, res: Response) => {
+    const response = await api.get(`Bildirim/${req.params.id}`)
+
+    const parsed = parseKapHtmlToMarkdown(response.data)
+
+    console.log(parsed)
+    
+    const analysis = await analyzeKapNotification(parsed);
+
+    return AppResponse(res, 'Duyuru başarıyla analiz edildi.', analysis)
 })
